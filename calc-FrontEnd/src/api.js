7c9ffead4c1235bca7f94b1ea28730d2af710d27
VenkataSubrahmanyamKavala <2300032378@kluniversity.in>
2025-08-25 16:40:06 +0530

    fsfs

--- calc-FrontEnd/src/api.js
+++ calc-FrontEnd/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 // export const BASEURL = "http://localhost:2025/";
-export const BASEURL = "http://localhost:8082/calc/";
+export const BASEURL = "http://localhost:8082/calc";
+
 export const callApi = async (method, endpoint) => {
   try {
     const response = await axios({
